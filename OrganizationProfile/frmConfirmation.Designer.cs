dce13e41e092f607be2982545e4914596a186a7b
debs gatchon <debsgatchon123@gmail.com>
2020-10-18 12:33:27 -0700

    initial commit

---
+++ OrganizationProfile/frmConfirmation.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace OrganizationProfile
+{
+    partial class frmConfirmation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudentNo1 = new System.Windows.Forms.Label();
+            this.lblName1 = new System.Windows.Forms.Label();
+            this.lblProgram1 = new System.Windows.Forms.Label();
+            this.lblAge1 = new System.Windows.Forms.Label();
+            this.lblBirthday1 = new System.Windows.Forms.Label();
+            this.lblGender1 = new System.Windows.Forms.Label();
+            this.lblContactNo1 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblStudentNo = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblProgram = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblBirthday = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblContactNo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStudentNo1
+            // 
+            this.lblStudentNo1.AutoSize = true;
+            this.lblStudentNo1.Location = new System.Drawing.Point(0, 0);
+            this.lblStudentNo1.Name = "lblStudentNo1";
+            this.lblStudentNo1.Size = new System.Drawing.Size(64, 13);
+            this.lblStudentNo1.TabIndex = 0;
+            this.lblStudentNo1.Text = "Student No:";
+            // 
+            // lblName1
+            // 
+            this.lblName1.AutoSize = true;
+            this.lblName1.Location = new System.Drawing.Point(0, 30);
+            this.lblName1.Name = "lblName1";
+            this.lblName1.Size = new System.Drawing.Size(38, 13);
+            this.lblName1.TabIndex = 1;
+            this.lblName1.Text = "Name:";
+            // 
+            // lblProgram1
+            // 
+            this.lblProgram1.AutoSize = true;
+            this.lblProgram1.Location = new System.Drawing.Point(0, 67);
+            this.lblProgram1.Name = "lblProgram1";
+            this.lblProgram1.Size = new System.Drawing.Size(49, 13);
+            this.lblProgram1.TabIndex = 2;
+            this.lblProgram1.Text = "Program:";
+            // 
+            // lblAge1
+            // 
+            this.lblAge1.AutoSize = true;
+            this.lblAge1.Location = new System.Drawing.Point(0, 107);
+            this.lblAge1.Name = "lblAge1";
+            this.lblAge1.Size = new System.Drawing.Size(29, 13);
+            this.lblAge1.TabIndex = 3;
+            this.lblAge1.Text = "Age:";
+            // 
+            // lblBirthday1
+            // 
+            this.lblBirthday1.AutoSize = true;
+            this.lblBirthday1.Location = new System.Drawing.Point(0, 144);
+            this.lblBirthday1.Name = "lblBirthday1";
+            this.lblBirthday1.Size = new System.Drawing.Size(48, 13);
+            this.lblBirthday1.TabIndex = 4;
+            this.lblBirthday1.Text = "Birthday:";
+            // 
+            // lblGender1
+            // 
+            this.lblGender1.AutoSize = true;
+            this.lblGender1.Location = new System.Drawing.Point(0, 177);
+            this.lblGender1.Name = "lblGender1";
+            this.lblGender1.Size = new System.Drawing.Size(45, 13);
+            this.lblGender1.TabIndex = 5;
+            this.lblGender1.Text = "Gender:";
+            // 
+            // lblContactNo1
+            // 
+            this.lblContactNo1.AutoSize = true;
+            this.lblContactNo1.Location = new System.Drawing.Point(0, 214);
+            this.lblContactNo1.Name = "lblContactNo1";
+            this.lblContactNo1.Size = new System.Drawing.Size(64, 13);
+            this.lblContactNo1.TabIndex = 6;
+            this.lblContactNo1.Text = "Contact No:";
+            this.lblContactNo1.Click += new System.EventHandler(this.lblContactNo1_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(90, 252);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblStudentNo
+            // 
+            this.lblStudentNo.AutoSize = true;
+            this.lblStudentNo.Location = new System.Drawing.Point(90, 0);
+            this.lblStudentNo.Name = "lblStudentNo";
+            this.lblStudentNo.Size = new System.Drawing.Size(35, 13);
+            this.lblStudentNo.TabIndex = 8;
+            this.lblStudentNo.Text = "label1";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(90, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "label2";
+            // 
+            // lblProgram
+            // 
+            this.lblProgram.AutoSize = true;
+            this.lblProgram.Location = new System.Drawing.Point(90, 67);
+            this.lblProgram.Name = "lblProgram";
+            this.lblProgram.Size = new System.Drawing.Size(35, 13);
+            this.lblProgram.TabIndex = 10;
+            this.lblProgram.Text = "label3";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(90, 107);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(35, 13);
+            this.lblAge.TabIndex = 11;
+            this.lblAge.Text = "label4";
+            // 
+            // lblBirthday
+            // 
+            this.lblBirthday.AutoSize = true;
+            this.lblBirthday.Location = new System.Drawing.Point(90, 144);
+            this.lblBirthday.Name = "lblBirthday";
+            this.lblBirthday.Size = new System.Drawing.Size(35, 13);
+            this.lblBirthday.TabIndex = 12;
+            this.lblBirthday.Text = "label5";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(87, 177);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(35, 13);
+            this.lblGender.TabIndex = 13;
+            this.lblGender.Text = "label6";
+            // 
+            // lblContactNo
+            // 
+            this.lblContactNo.AutoSize = true;
+            this.lblContactNo.Location = new System.Drawing.Point(90, 214);
+            this.lblContactNo.Name = "lblContactNo";
+            this.lblContactNo.Size = new System.Drawing.Size(35, 13);
+            this.lblContactNo.TabIndex = 14;
+            this.lblContactNo.Text = "label7";
+            // 
+            // frmConfirmation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblContactNo);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblBirthday);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblProgram);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblStudentNo);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblContactNo1);
+            this.Controls.Add(this.lblGender1);
+            this.Controls.Add(this.lblBirthday1);
+            this.Controls.Add(this.lblAge1);
+            this.Controls.Add(this.lblProgram1);
+            this.Controls.Add(this.lblName1);
+            this.Controls.Add(this.lblStudentNo1);
+            this.Name = "frmConfirmation";
+            this.Text = "frmConfirmation";
+            this.Load += new System.EventHandler(this.frmConfirmation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudentNo1;
+        private System.Windows.Forms.Label lblName1;
+        private System.Windows.Forms.Label lblProgram1;
+        private System.Windows.Forms.Label lblAge1;
+        private System.Windows.Forms.Label lblBirthday1;
+        private System.Windows.Forms.Label lblGender1;
+        private System.Windows.Forms.Label lblContactNo1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblStudentNo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblProgram;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblBirthday;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblContactNo;
+    }
+}
